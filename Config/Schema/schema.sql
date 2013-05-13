34f503e61e425b03126aba12f85eed2ac86f479b
Jimmy <admin@jimmymadon.com>
2013-05-13 15:31:55 +0100

    Added fields for to_name and sent_content.

--- Config/Schema/schema.sql
+++ Config/Schema/schema.sql
@@ -1,7 +1,8 @@
 DROP TABLE IF EXISTS `email_queue`;
-CREATE TABLE IF NOT EXISTS `email_queue` (
+CREATE TABLE `email_queue` (
   `id` char(36) CHARACTER SET ascii NOT NULL,
-  `to` varchar(129) NOT NULL,
+  `to_name` varchar(129) NOT NULL,
+  `to_email` varchar(129) NOT NULL,
   `from_name` varchar(255) NOT NULL,
   `from_email` varchar(255) NOT NULL,
   `subject` varchar(255) NOT NULL,
@@ -11,6 +12,7 @@ CREATE TABLE IF NOT EXISTS `email_queue` (
   `format` varchar(5) NOT NULL,
   `template_vars` text NOT NULL,
   `sent` tinyint(1) NOT NULL,
+  `sent_content` text,
   `locked` tinyint(1) NOT NULL DEFAULT '0',
   `send_tries` int(2) NOT NULL,
   `send_at` datetime DEFAULT NULL,
